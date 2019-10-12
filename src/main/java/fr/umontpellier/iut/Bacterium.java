383fe7f7bdcbf878de15806723ce8eafab521c11
Jorda-Lea <lea.jorda@etu.umontpellier.fr>
2019-10-12 16:08:47 +0200

    Modification dans Bacterium
    Squelette de move() et de computeConcentrationInDirection() à compléter
    getConsentration() necessaire dans Cell
    getTimeDelta() necessaire dans Environment

--- src/main/java/fr/umontpellier/iut/Bacterium.java
+++ src/main/java/fr/umontpellier/iut/Bacterium.java
@@ -15,14 +15,40 @@ public class Bacterium {
      * Uses computeConcentrationInDirection()
      * (more info : #8 of description.pdf)
      */
-    public void move() {}
+    public void move() {
+        int vx = (/*vd*/*(computeConcentrationInDirection("ouest")-computeConcentrationInDirection("est")))/(2*/*longueur de la cellule*/);
+        x = x + (environment/*.getTimeDelta*/*vx) + bDiff*Math.sqrt(environment./*.getTimeDelta*/)*Math.random();
+
+        int vy = (/*vd*/*(computeConcentrationInDirection("sud")-computeConcentrationInDirection("nord")))/(2*/*longueur de la cellule*/);
+        y = y + (environment/*.getTimeDelta*/*vy) + bDiff*Math.sqrt(environment./*.getTimeDelta*/)*Math.random();
+
+    }
 
     /**
      * @param direction
      * @return the concentration C of the cells in the "direction"
      * (more info : #8 of description.pdf)
      */
-    public double computeConcentrationInDirection(String direction) {return 0.0;}
+    public double computeConcentrationInDirection(String direction) {
+        double concentration;
+        switch (direction) {
+            case "ouest" :
+                concentration = ;// getConcentration() de la cellule de gauche
+                break;
+            case "est" :
+                concentration = ;// getConcentration() de la cellule de droite
+                break;
+            case "sud" :
+                concentration = ;// getConcentration() de la cellule au-dessous
+                break;
+            case "nord" :
+                concentration = ;// getConcentration() de la cellule au-dessus
+                break;
+
+        }
+        return concentration;
+
+    }
 
     /**
      * Lowers the concentration in the 9 neighboring cells

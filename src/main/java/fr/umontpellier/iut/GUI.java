f3aac76164902650a69cffdce3c1289c5c54628c
Tristan-T <43269571+Tristan-T@users.noreply.github.com>
2019-11-02 15:39:33 +0100

    Added Windows instructions for OpenJFX

--- src/main/java/fr/umontpellier/iut/GUI.java
+++ src/main/java/fr/umontpellier/iut/GUI.java
@@ -3,6 +3,14 @@ package fr.umontpellier.iut;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+// During dev
+// For Linux : install openjfx package (by default with OpenJDK)
+// For Windows : Download JavaFX Windows SDK (>=11) from here : https://gluonhq.com/products/javafx/
+// Extract it somewhere safe
+// In IntelliJ IDEA : File -> Project Structure -> Modules -> Dependencies -> + (left side button) -> Jar or directory -> Select the libs folder inside the file you extracted
+// Run the GUI.main();
+// Once the app ships, it'll package everything
+
 public class GUI extends Application {
 
     @Override

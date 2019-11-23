9405489b8b0c30f1a95afc09c4e98837b16412db
PierreYves-Boulay <pierre-yves.boulay@etu.umontpellier.fr>
2019-11-23 13:00:55 +0100

    Moved config-related methods to the appropriate class

--- src/main/java/fr/umontpellier/iut/Settings.java
+++ src/main/java/fr/umontpellier/iut/Settings.java
@@ -1,5 +1,15 @@
 package fr.umontpellier.iut;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Settings {
     /**
      * WE WILL SET AND GET THE DIFFERENT PARAMETERS THROUGH THIS CLASS FOR EASE OF USE
@@ -52,4 +62,111 @@ public class Settings {
     public static int getSimulation_timeDeltaSubdivision() { return Simulation.getTimeDeltaSubdivision(); }
     public static int getSimulation_initialBacteriaAmount() { return Simulation.getInitialBacteriaAmount(); }
     public static double getSimulation_maxDuration() { return Simulation.getMaxDuration(); }
+
+    //CONFIG INTERACTION
+    public static void exportConfig(String fileName){
+        //Create the various JSON objects and fill them with appropriate values
+        JSONObject simulationSettings = new JSONObject();
+        simulationSettings.put("timeDelta", Settings.getSimulation_timeDelta());
+        simulationSettings.put("timeDeltaSubdivision", Settings.getSimulation_timeDeltaSubdivision());
+        simulationSettings.put("initialBacteriaAmount", Settings.getSimulation_initialBacteriaAmount());
+        simulationSettings.put("maxDuration", Settings.getSimulation_maxDuration());
+
+        JSONObject simulation = new JSONObject();
+        simulation.put("Simulation", simulationSettings);
+
+        JSONObject environmentSettings = new JSONObject();
+        environmentSettings.put("cellsPerSide", Settings.getEnvironment_cellsPerSide());
+        environmentSettings.put("halfLength", Settings.getEnvironment_halfLength());
+        environmentSettings.put("substratumRadius", Settings.getEnvironment_substratumRadius());
+
+        JSONObject environment = new JSONObject();
+        environment.put("Environment", environmentSettings);
+
+        JSONObject cellSettings = new JSONObject();
+        cellSettings.put("cIni", Settings.getCell_cIni());
+        cellSettings.put("cMin", Settings.getCell_cMin());
+        cellSettings.put("vDiff", Settings.getCell_vDiff());
+
+        JSONObject cell = new JSONObject();
+        cell.put("Cell", cellSettings);
+
+        JSONObject bacteriumSettings = new JSONObject();
+        bacteriumSettings.put("bDiff", Settings.getBacterium_bDiff());
+        bacteriumSettings.put("kConv", Settings.getBacterium_kConv());
+        bacteriumSettings.put("mIni", Settings.getBacterium_mIni());
+        bacteriumSettings.put("vCons", Settings.getBacterium_vCons());
+        bacteriumSettings.put("vd", Settings.getBacterium_vd());
+
+        JSONObject bacterium = new JSONObject();
+        bacterium.put("Bacterium", bacteriumSettings);
+
+        //Add all categories to list
+        JSONArray config = new JSONArray();
+        config.add(simulation);
+        config.add(environment);
+        config.add(cell);
+        config.add(bacterium);
+
+        //Write JSON file in the config folder
+        try (FileWriter file = new FileWriter("./config/"+fileName+".json")) {
+
+            file.write(config.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadConfig(String fileName){
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("./config/"+fileName+".json"))
+        {
+            //Read JSON file and cast it as JSONArray
+            Object obj = jsonParser.parse(reader);
+            JSONArray settings = (JSONArray) obj;
+
+            //Get the category then the setting and apply it
+            JSONObject simulation = (JSONObject) settings.get(0);
+            simulation= (JSONObject) simulation.get("Simulation");
+
+            //We have to cast what we get because it returns an object
+            Settings.setSimulation_timeDelta((double) simulation.get("timeDelta"));
+            Settings.setSimulation_timeDeltaSubdivision((int) (long) simulation.get("timeDeltaSubdivision"));
+            Settings.setSimulation_initialBacteriaAmount((int) (long) simulation.get("initialBacteriaAmount"));
+            Settings.setSimulation_maxDuration((double) simulation.get("maxDuration"));
+
+            JSONObject environment = (JSONObject) settings.get(1);
+            environment= (JSONObject) environment.get("Environment");
+
+            Settings.setEnvironment_cellsPerSide((int) (long) environment.get("cellsPerSide"));
+            Settings.setEnvironment_halfLength((double) environment.get("halfLength"));
+            Settings.setEnvironment_substratumRadius((double) environment.get("substratumRadius"));
+
+            JSONObject cell = (JSONObject) settings.get(2);
+            cell= (JSONObject) cell.get("Cell");
+
+            Settings.setCell_cIni((double) cell.get("cIni"));
+            Settings.setCell_cMin((double) cell.get("cMin"));
+            Settings.setCell_vDiff((double) cell.get("vDiff"));
+
+            JSONObject bacterium = (JSONObject) settings.get(3);
+            bacterium= (JSONObject) bacterium.get("Bacterium");
+
+            Settings.setBacterium_bDiff((double) bacterium.get("bDiff"));
+            Settings.setBacterium_mIni((double) bacterium.get("mIni"));
+            Settings.setBacterium_vCons((double) bacterium.get("vCons"));
+            Settings.setBacterium_kConv((double) bacterium.get("kConv"));
+            Settings.setBacterium_vd((double) bacterium.get("vd"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }

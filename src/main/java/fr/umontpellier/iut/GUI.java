3f5b186a78858e99ee1e8097da3733e881223ac2
Tristan-T <43269571+Tristan-T@users.noreply.github.com>
2019-12-01 23:01:07 +0100

    Added tabs Added basic run menu

--- src/main/java/fr/umontpellier/iut/GUI.java
+++ src/main/java/fr/umontpellier/iut/GUI.java
@@ -1,18 +1,30 @@
 package fr.umontpellier.iut;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class GUI extends Application {
 
@@ -23,46 +35,237 @@ public class GUI extends Application {
      * window.
      */
 
-    private static GridPane settingsPane() {
-        GridPane settingsBox = new GridPane();
-        settingsBox.setPadding(new Insets(10, 10, 10, 10));
-        settingsBox.setVgap(10);
-        settingsBox.setHgap(70);
-        settingsBox.getColumnConstraints().add(new ColumnConstraints(400));
 
-        SpinnerSetter cIni = new SpinnerSetter("cIni", 0, 200, 10, "toDefine");
+    //Getting screen properties
+    private static Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+
+
+    private TabPane settingTabs() {
+
+        TabPane settingsBox = new TabPane();
+        settingsBox.setPadding(new Insets(10, 10, 10, 10));
+
+        // Simulation
+        SpinnerSetter timeDelta = new SpinnerSetter("timeDelta", 0, 200, 10, "toDefine");
+        SpinnerSetter initialBacteriaAmount = new SpinnerSetter("initialBacteriaAmount", 0, 200, 10, "toDefine");
+        SpinnerSetter timeDeltaSubdivision = new SpinnerSetter("timeDeltaSubdivision", 0, 200, 10, "toDefine");
+        SpinnerSetter maxDuration = new SpinnerSetter("maxDuration", 0, 200, 10, "toDefine");
+
+        // Environment
+        SpinnerSetter halfLength = new SpinnerSetter("halfLength", 0, 200, 10, "toDefine");
+        SpinnerSetter cellsPerSide = new SpinnerSetter("cellsPerSide", 0, 200, 10, "toDefine");
+        SpinnerSetter substratumRadius = new SpinnerSetter("substratumRadius", 0, 200, 10, "toDefine");
+
+        // Cell
         SpinnerSetter cMin = new SpinnerSetter("cMin", 0, 200, 10, "toDefine");
         SpinnerSetter vDiff = new SpinnerSetter("vDiff", 0, 200, 10, "toDefine");
-        SpinnerSetter cellsPerSide = new SpinnerSetter("cellsPerSide", 0, 200, 10, "toDefine");
-        SpinnerSetter halfLength = new SpinnerSetter("halfLength", 0, 200, 10, "toDefine");
-        SpinnerSetter substratumRadius = new SpinnerSetter("substratumRadius", 0, 200, 10, "toDefine");
+        SpinnerSetter cIni = new SpinnerSetter("cIni", 0, 200, 10, "toDefine");
+
+        // Bacterium
         SpinnerSetter bDiff = new SpinnerSetter("bDiff", 0, 200, 10, "toDefine");
         SpinnerSetter mIni = new SpinnerSetter("mIni", 0, 200, 10, "toDefine");
         SpinnerSetter vCons = new SpinnerSetter("vCons", 0, 200, 10, "toDefine");
-        SpinnerSetter vd = new SpinnerSetter("vd", 0, 200, 10, "toDefine");
         SpinnerSetter kConv = new SpinnerSetter("kConv", 0, 200, 10, "toDefine");
-        SpinnerSetter timeDelta = new SpinnerSetter("timeDelta", 0, 200, 10, "toDefine");
-        SpinnerSetter timeDeltaSubdivision = new SpinnerSetter("timeDeltaSubdivision", 0, 200, 10, "toDefine");
-        SpinnerSetter initialBacteriaAmount = new SpinnerSetter("initialBacteriaAmount", 0, 200, 10, "toDefine");
-        SpinnerSetter maxDuration = new SpinnerSetter("maxDuration", 0, 200, 10, "toDefine");
+        SpinnerSetter vd = new SpinnerSetter("vd", 0, 200, 10, "toDefine");
 
-        settingsBox.add(cIni.getSpinnerSetter(), 0, 0);
-        settingsBox.add(cMin.getSpinnerSetter(), 0, 1);
-        settingsBox.add(vDiff.getSpinnerSetter(), 0, 2);
-        settingsBox.add(cellsPerSide.getSpinnerSetter(), 0, 3);
-        settingsBox.add(halfLength.getSpinnerSetter(), 0, 4);
-        settingsBox.add(substratumRadius.getSpinnerSetter(), 0, 5);
-        settingsBox.add(bDiff.getSpinnerSetter(), 0, 6);
-        settingsBox.add(mIni.getSpinnerSetter(), 0, 7);
-        settingsBox.add(vCons.getSpinnerSetter(), 0, 8);
-        settingsBox.add(vd.getSpinnerSetter(), 0, 9);
-        settingsBox.add(kConv.getSpinnerSetter(), 0, 10);
-        settingsBox.add(timeDelta.getSpinnerSetter(), 0, 11);
-        settingsBox.add(timeDeltaSubdivision.getSpinnerSetter(), 0, 12);
-        settingsBox.add(initialBacteriaAmount.getSpinnerSetter(), 0, 13);
-        settingsBox.add(maxDuration.getSpinnerSetter(), 0, 14);
+        // Creating pane to correctly place setters per categories
+        GridPane simulationGridPane = new GridPane();
+        simulationGridPane.maxWidth(Double.MAX_VALUE);
+        GridPane environmentGridPane = new GridPane();
+        GridPane cellGridPane = new GridPane();
+        GridPane bacteriumGridPane = new GridPane();
+
+        //List for easier aesthetic manipulations
+        ArrayList<GridPane> gridPanes = new ArrayList<>();
+        gridPanes.add(simulationGridPane);
+        gridPanes.add(environmentGridPane);
+        gridPanes.add(cellGridPane);
+        gridPanes.add(bacteriumGridPane);
+
+        for (GridPane g: gridPanes) {
+            g.setVgap(10);
+            g.setHgap(70);
+            g.getColumnConstraints().add(new ColumnConstraints(600));
+            g.setMinHeight(screenBounds.getHeight()/1.5);
+            g.setMinWidth(screenBounds.getWidth()/4);
+        }
+
+        // Creating tabs to make changing settings easier
+        Tab cellTab = new Tab("Cells", cellGridPane);
+        Tab environmentTab = new Tab("Environment", environmentGridPane);
+        Tab bacteriumTab = new Tab("Bacterium", bacteriumGridPane);
+        Tab simulationTab = new Tab("Simulation", simulationGridPane);
+
+        // Adding each element to their position in their tabs
+        // Simulation
+        simulationGridPane.add(timeDelta.getSpinnerSetter(), 0, 0);
+        simulationGridPane.add(initialBacteriaAmount.getSpinnerSetter(), 0, 1);
+        simulationGridPane.add(timeDeltaSubdivision.getSpinnerSetter(), 0, 2);
+        simulationGridPane.add(maxDuration.getSpinnerSetter(), 0, 3);
+
+        // Environment
+        environmentGridPane.add(halfLength.getSpinnerSetter(), 0, 0);
+        environmentGridPane.add(cellsPerSide.getSpinnerSetter(), 0, 1);
+        environmentGridPane.add(substratumRadius.getSpinnerSetter(), 0, 2);
+
+        // Cells
+        cellGridPane.add(cMin.getSpinnerSetter(), 0, 0);
+        cellGridPane.add(vDiff.getSpinnerSetter(), 0, 1);
+        cellGridPane.add(cIni.getSpinnerSetter(), 0, 2);
+
+        // Bacterium
+        bacteriumGridPane.add(bDiff.getSpinnerSetter(), 0, 0);
+        bacteriumGridPane.add(mIni.getSpinnerSetter(), 0, 1);
+        bacteriumGridPane.add(vCons.getSpinnerSetter(), 0, 2);
+        bacteriumGridPane.add(kConv.getSpinnerSetter(), 0, 3);
+        bacteriumGridPane.add(vd.getSpinnerSetter(), 0, 4);
+
+        settingsBox.getTabs().add(simulationTab);
+        settingsBox.getTabs().add(environmentTab);
+        settingsBox.getTabs().add(cellTab);
+        settingsBox.getTabs().add(bacteriumTab);
+
+        settingsBox.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
         return settingsBox;
+    }
+
+    private GridPane launcherBox(Stage stage) throws IOException {
+        GridPane launcher = new GridPane();
+
+        SpinnerSetter numberRun = new SpinnerSetter("Nombre d'exécutions", 0, 100, 1, "tours");
+
+        InputStream streamRun = getClass().getResourceAsStream("/run.png");
+        Image runImage = new Image(streamRun);
+        Button runButton = new Button("Lancer", new ImageView(runImage));
+        runButton.setDefaultButton(true);
+
+        InputStream streamStop = getClass().getResourceAsStream("/stop.png");
+        Image stopImage = new Image(streamStop);
+        Button stopButton = new Button("Arrêter", new ImageView(stopImage));
+        stopButton.setCancelButton(true);
+
+        //File filter
+        FileChooser.ExtensionFilter jsonFilter = new FileChooser.ExtensionFilter("Configuration files (*.json)", "*.json");
+
+        Button importButton = new Button("Import config");
+
+        importButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                FileChooser settingImporter = new FileChooser();
+                //Set filter
+                settingImporter.getExtensionFilters().add(jsonFilter);
+                File selectedFile = settingImporter.showOpenDialog(null);
+                settingImporter.setTitle("Choose a configuration file");
+
+
+                if (selectedFile != null) {
+
+
+                    Alert importConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+                    importConfirmation.setTitle("Confirmation Dialog");
+                    importConfirmation.setHeaderText("Importing this configuration will erase your previous configuration");
+
+                    Optional<ButtonType> result = importConfirmation.showAndWait();
+                    if (result.get() == ButtonType.OK){
+                        Alert importSuccessful = new Alert(Alert.AlertType.INFORMATION);
+                        importSuccessful.setTitle("Configuration imported");
+                        importSuccessful.setHeaderText(null);
+                        importSuccessful.setContentText("File imported : " + selectedFile.getName());
+                        /*
+                        * MUST IMPORT THE CONFIGURATION HERE
+                         */
+
+                        importSuccessful.showAndWait();
+                    } else {
+                        Alert importCancel = new Alert(Alert.AlertType.INFORMATION);
+                        importCancel.setTitle("Cancelled");
+                        importCancel.setHeaderText(null);
+                        importCancel.setContentText("Import was cancelled");
+
+                        importCancel.showAndWait();
+                    }
+
+                }
+                else {
+                    Alert importCancel = new Alert(Alert.AlertType.INFORMATION);
+                    importCancel.setTitle("Cancelled");
+                    importCancel.setHeaderText(null);
+                    importCancel.setContentText("Import was cancelled");
+
+                    importCancel.showAndWait();
+                }
+            }
+        });
+
+
+        Button exportButton = new Button("Save configuration");
+        exportButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                FileChooser settingExporter = new FileChooser();
+                settingExporter.setTitle("Save your configuration");
+                //Set extension filter for text files
+                settingExporter.getExtensionFilters().add(jsonFilter);
+                File savedFile = settingExporter.showSaveDialog(stage);
+
+
+
+                if (savedFile != null) {
+                    /*
+                     * EXPORTING THE CONFIGURATION NEEDS TO BE HERE
+                     * MUST ADD THE CONFIGURATION RETRIEVE PROCESS
+                     */
+                    Settings.exportConfig(savedFile.getName());
+                    /* Doesn't want to catch IOException, must check later
+                    try {
+
+                    }
+
+                    catch(IOException e) {
+
+                    e.printStackTrace();
+                    Alert saveError = new Alert(Alert.AlertType.ERROR);
+                    saveError.setTitle("Export error");
+                    saveError.setHeaderText(null);
+                    saveError.setContentText("Can't export configuration");
+
+                    saveError.showAndWait();
+
+                    throw new IOException("Something happened");
+                    }
+                    */
+
+                    Alert saveSuccessful = new Alert(Alert.AlertType.INFORMATION);
+                    saveSuccessful.setTitle("Configuration exported");
+                    saveSuccessful.setHeaderText(null);
+                    saveSuccessful.setContentText("Your configuration was exported");
+
+                    saveSuccessful.showAndWait();
+
+                } else {
+                    Alert saveCancel = new Alert(Alert.AlertType.INFORMATION);
+                    saveCancel.setTitle("Cancelled");
+                    saveCancel.setHeaderText(null);
+                    saveCancel.setContentText("Export was cancelled");
+
+                    saveCancel.showAndWait();
+                }
+            }
+        });
+
+
+
+        launcher.add(numberRun.getSpinnerSetter(), 0, 0);
+        launcher.add(importButton, 0, 1);
+        launcher.add(exportButton, 1, 1);
+        launcher.add(runButton, 0, 2);
+        launcher.add(stopButton, 1, 2);
+
+
+
+        return launcher;
     }
 
     private static void launchingPane(Scene scene) {
@@ -83,12 +286,9 @@ public class GUI extends Application {
         Image image = new Image(iconStream);
         stage.getIcons().add(image);
 
-        //Getting screen properties
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-
         //Setting window size
-        stage.setX(screenBounds.getWidth()/4);
-        stage.setY(screenBounds.getHeight()/4);
+        stage.setX(screenBounds.getWidth()/16);
+        stage.setY(screenBounds.getHeight()/16);
         stage.setMaxWidth(screenBounds.getWidth());
         stage.setMaxHeight(screenBounds.getHeight());
         stage.setMinHeight(screenBounds.getHeight()/4);
@@ -102,8 +302,20 @@ public class GUI extends Application {
             }
         });
 
+        VBox leftSide = new VBox();
+        leftSide.getChildren().add(settingTabs());
+        leftSide.getChildren().add(launcherBox(stage));
+
+
+
+        //Creating and adding settings to the scene
+        Scene mainScene = new Scene(leftSide);
+
+
+
         //Adding the scene to the stage
-        stage.setScene(new Scene(settingsPane()));
+        stage.setScene(mainScene);
+
 
         stage.show();
     }
